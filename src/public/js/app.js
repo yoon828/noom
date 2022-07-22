3db162f5bc80bf0af3d7788d7a8e706f9ef7b2e6
김윤민 <k915k1004@naver.com>
2022-07-22 22:43:12 +0900

    websocket으로 구현

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,14 +1,25 @@
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
+
 //socket : 서버로의 연결
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
 
 //서버와 연결
 socket.addEventListener("open", () => {
   console.log("Connected to Server ✅");
 });
 
-//메시지
+//메시지 받기
 socket.addEventListener("message", (message) => {
-  console.log("New message : ", message.data, "from the Server");
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 //서버 연결 종료
@@ -16,6 +27,21 @@ socket.addEventListener("close", () => {
   console.log("Disconnected from Server ❌");
 });
 
-setTimeout(() => {
-  socket.send("hello from the browser!");
-}, 5000);
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(makeMessage("new_message", input.value));
+  const li = document.createElement("li");
+  li.innerText = `You : ${input.value}`;
+  messageList.append(li);
+  input.value = "";
+}
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+  input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -17,22 +17,36 @@ const server = http.createServer(app); //http 서버
 // http서버 위에 wss만들기
 const wss = new WebSocket.Server({ server }); //webSocket 서버 + http 서버를 합쳐서 하나의 포트를 사용하기 위해서
 
+const sockets = [];
+
 wss.on("connection", (socket) => {
   //socket : 연결된 브라우저
+
+  //연결된 브라우저들을 sockets 배열에 넣어주기
+  sockets.push(socket);
+  socket["nickname"] = "Anon"; //닉네임을 설정하지 않은 경우
   console.log("Connected to browser ✅");
-  console.log(socket);
   //브라우저와 연결 종료
   socket.on("close", () => {
     console.log("Disconnected from the Browser ❌");
   });
 
   //브라우저에서 메세지 받기
-  socket.on("message", (message) => {
-    console.log(message.toString());
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname} : ${message.payload}`)
+        );
+      case "nickname":
+        //socket 객체에 nickname을 추가
+        socket["nickname"] = message.payload;
+    }
   });
 
   //브라우저에 메시지 보내기
-  socket.send("hello");
+  // socket.send("hello");
 });
 
 server.listen(3000, handleListen);
