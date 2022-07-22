071a4efb445bf1401f342cf9f7b592eaa4936f36
김윤민 <k915k1004@naver.com>
2022-07-23 01:20:18 +0900

    message broadcase

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,47 +1,55 @@
-const messageList = document.querySelector("ul");
-const nickForm = document.querySelector("#nick");
-const messageForm = document.querySelector("#message");
+const socket = io();
 
-//socket : 서버로의 연결
-const socket = new WebSocket(`ws://${window.location.host}`);
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+const room = document.getElementById("room");
 
-function makeMessage(type, payload) {
-  const msg = { type, payload };
-  return JSON.stringify(msg);
+room.hidden = true;
+
+let roomName;
+
+function addMessage(message) {
+  const ul = room.querySelector("ul");
+  const li = document.createElement("li");
+  li.innerText = message;
+  ul.appendChild(li);
 }
 
-//서버와 연결
-socket.addEventListener("open", () => {
-  console.log("Connected to Server ✅");
-});
-
-//메시지 받기
-socket.addEventListener("message", (message) => {
-  const li = document.createElement("li");
-  li.innerText = message.data;
-  messageList.append(li);
-});
-
-//서버 연결 종료
-socket.addEventListener("close", () => {
-  console.log("Disconnected from Server ❌");
-});
-
-function handleSubmit(event) {
+function handleMessageSubmit(event) {
   event.preventDefault();
-  const input = messageForm.querySelector("input");
-  socket.send(makeMessage("new_message", input.value));
-  const li = document.createElement("li");
-  li.innerText = `You : ${input.value}`;
-  messageList.append(li);
-  input.value = "";
-}
-function handleNickSubmit(event) {
-  event.preventDefault();
-  const input = nickForm.querySelector("input");
-  socket.send(makeMessage("nickname", input.value));
+  const input = room.querySelector("input");
+  const value = input.value;
+  socket.emit("new_message", input.value, roomName, () => {
+    addMessage(`You: ${value}`);
+  });
   input.value = "";
 }
 
-messageForm.addEventListener("submit", handleSubmit);
-nickForm.addEventListener("submit", handleNickSubmit);
+function showRoom() {
+  welcome.hidden = true;
+  room.hidden = false;
+  const h3 = room.querySelector("h3");
+  h3.innerText = `Room ${roomName}`;
+  const form = room.querySelector("form");
+  form.addEventListener("submit", handleMessageSubmit);
+}
+
+function handleRoomSubmit(event) {
+  event.preventDefault();
+  const input = form.querySelector("input");
+  socket.emit("enter_room", input.value, showRoom);
+  roomName = input.value;
+  input.value = "";
+}
+
+form.addEventListener("submit", handleRoomSubmit);
+
+socket.on("welcome", () => {
+  addMessage("someone joined!");
+});
+
+socket.on("bye", () => {
+  addMessage("someone left ㅠㅠ");
+});
+
+socket.on("new_message", addMessage);
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
-import express from "express";
 import http from "http";
-import WebSocket from "ws";
+import { Server } from "socket.io";
+import express from "express";
 
 const app = express();
 
@@ -11,42 +11,59 @@ app.get("/", (req, res) => res.render("home"));
 //어떤 경로를 들어와도 항상 /로 리다이렉트
 app.get("/*", (req, res) => res.redirect("/"));
 
-const handleListen = () => console.log("Listening ong http://localhost:3000");
+const httpServer = http.createServer(app); //http 서버
+const wsServer = new Server(httpServer);
 
-const server = http.createServer(app); //http 서버
-// http서버 위에 wss만들기
-const wss = new WebSocket.Server({ server }); //webSocket 서버 + http 서버를 합쳐서 하나의 포트를 사용하기 위해서
-
-const sockets = [];
-
-wss.on("connection", (socket) => {
-  //socket : 연결된 브라우저
-
-  //연결된 브라우저들을 sockets 배열에 넣어주기
-  sockets.push(socket);
-  socket["nickname"] = "Anon"; //닉네임을 설정하지 않은 경우
-  console.log("Connected to browser ✅");
-  //브라우저와 연결 종료
-  socket.on("close", () => {
-    console.log("Disconnected from the Browser ❌");
+wsServer.on("connection", (socket) => {
+  socket.onAny((event) => {
+    console.log(`Socket Event: ${event}`);
   });
-
-  //브라우저에서 메세지 받기
-  socket.on("message", (msg) => {
-    const message = JSON.parse(msg);
-    switch (message.type) {
-      case "new_message":
-        sockets.forEach((aSocket) =>
-          aSocket.send(`${socket.nickname} : ${message.payload}`)
-        );
-      case "nickname":
-        //socket 객체에 nickname을 추가
-        socket["nickname"] = message.payload;
-    }
+  socket.on("enter_room", (roomName, done) => {
+    socket.join(roomName);
+    done();
+    socket.to(roomName).emit("welcome");
   });
-
-  //브라우저에 메시지 보내기
-  // socket.send("hello");
+  socket.on("disconnecting", () => {
+    socket.rooms.forEach((room) => socket.to(room).emit("bye"));
+  });
+  socket.on("new_message", (msg, room, done) => {
+    socket.to(room).emit("new_message", msg);
+    done();
+  });
 });
 
-server.listen(3000, handleListen);
+// // http서버 위에 wss만들기
+// const wss = new WebSocket.Server({ server }); //webSocket 서버 + http 서버를 합쳐서 하나의 포트를 사용하기 위해서
+// const sockets = [];
+// wss.on("connection", (socket) => {
+//   //socket : 연결된 브라우저
+
+//   //연결된 브라우저들을 sockets 배열에 넣어주기
+//   sockets.push(socket);
+//   socket["nickname"] = "Anon"; //닉네임을 설정하지 않은 경우
+//   console.log("Connected to browser ✅");
+//   //브라우저와 연결 종료
+//   socket.on("close", () => {
+//     console.log("Disconnected from the Browser ❌");
+//   });
+
+//   //브라우저에서 메세지 받기
+//   socket.on("message", (msg) => {
+//     const message = JSON.parse(msg);
+//     switch (message.type) {
+//       case "new_message":
+//         sockets.forEach((aSocket) =>
+//           aSocket.send(`${socket.nickname} : ${message.payload}`)
+//         );
+//       case "nickname":
+//         //socket 객체에 nickname을 추가
+//         socket["nickname"] = message.payload;
+//     }
+//   });
+
+//   //브라우저에 메시지 보내기
+//   // socket.send("hello");
+// });
+
+const handleListen = () => console.log("Listening ong http://localhost:3000");
+httpServer.listen(3000, handleListen);
